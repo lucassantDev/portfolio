fac1e599cfb288b4da8b398f51401ccbdf821e32
lucassant <lucasfdesantana095@gmail.com>
2026-05-12 11:25:58 -0300

    style: adicionando animacao nos icones

--- portfolio-react/src/components/Skills.jsx
+++ portfolio-react/src/components/Skills.jsx
@@ -6,6 +6,7 @@ import { SiN8N } from "react-icons/si";
 import { RiPhpFill } from "react-icons/ri";
 import { FaJsSquare } from "react-icons/fa";
 import { SiTailwindcss } from "react-icons/si";
+import { FaLaravel } from "react-icons/fa6";
 
 
 function Skills(){
@@ -14,6 +15,48 @@ function Skills(){
             <section className="section-skills">
                 <h1>Skills</h1>
                 <div className='div-stack '>
+                    <div className="stack">
+                        <FaReact
+                        className="icon"
+                        />
+                        <p className="stack-name">React</p>
+                    </div>
+                    <div className="stack">
+                        <FiFigma
+                        className="icon"
+                        />
+                        <p className="stack-name">Figma</p>
+                    </div>
+                    <div className="stack">
+                        <SiN8N
+                        className="icon"
+                        />
+                        <p className="stack-name">N8N</p>
+                    </div>
+                    <div className="stack">
+                        <RiPhpFill
+                        className="icon"
+                        />
+                        <p className="stack-name">PHP</p>
+                    </div>
+                    <div className="stack">
+                        <FaJsSquare
+                        className="icon"
+                        />
+                        <p className="stack-name">JS</p>
+                    </div>
+                    <div className="stack">
+                        <SiTailwindcss
+                        className="icon"
+                        />
+                        <p className="stack-name">Tailwind</p>
+                    </div>
+                    <div className="stack">
+                        <FaLaravel
+                        className="icon"
+                        />
+                        <p className="stack-name">Laravel</p>
+                    </div>
                 </div>
             </section>
         </>
