f5116554012bbcd14a957360dd38e6adcaf5cf78
lucassant <lucasfdesantana095@gmail.com>
2026-05-12 12:05:55 -0300

    feat: adicionando ano dinamico para o footer

--- portfolio-react/src/components/Footer.jsx
+++ portfolio-react/src/components/Footer.jsx
@@ -1,11 +1,15 @@
 import '../style/Footer.css'
 
 function Footer(){
+
+    // pequeno script para o ano dinâmico
+    let anoAtual = new Date().getFullYear();
+
     return(
         <>
             <footer className="footer">
                 <p>
-                    © 2025 - lucassantDev Portfólio
+                    © {anoAtual} - lucassantDev Portfólio
                 </p>
             </footer>
         
