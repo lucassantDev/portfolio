b7d426624c98455ee9365e90bd7afe374ec32a2f
lucassant <lucasfdesantana095@gmail.com>
2026-05-26 17:31:14 -0300

    refactor: animacao com tailwind na section de Skills

--- portfolio-react/src/components/Skills.jsx
+++ portfolio-react/src/components/Skills.jsx
@@ -13,52 +13,52 @@ function Skills(){
     return (
         <>
             <section className="gap-8 w-full border-2 h-screen bg-[var(--color01)] flex flex-col items-center justify-center md:gap-12">
-                <h1 class="text-2xl font-semibold text-[var(--color03)] md:font-semibold md:text-7xl ">Skills</h1>
+                <h1 className="text-2xl font-semibold text-[var(--color03)] md:font-semibold md:text-7xl">Skills</h1>
                 <div className='w-1/2 grid grid-cols-2 gap-6 md:w-80 md:gap-12'>
 
-                    <div className="flex flex-col items-center text-[var(--color03)]">
+                    <div className="skill-card mr-3 flex flex-col items-center text-[var(--color03)]">
                         <FaReact
-                        className="text-4xl md:text-6xl"
+                        className=" text-4xl md:text-6xl"
                         />
                         <p className="text-sm md:text-2xl">React</p>
                     </div>
 
-                    <div className="flex flex-col items-center text-[var(--color03)]">
+                    <div className="skill-card mr-4 flex flex-col items-center text-[var(--color03)]">
                         <FiFigma
                         className="text-4xl md:text-6xl "
                         />
                         <p className="text-sm md:text-2xl">Figma</p>
                     </div>
 
-                    <div className="flex flex-col items-center text-[var(--color03)]">
+                    <div className="skill-card mr-5 flex flex-col items-center text-[var(--color03)]">
                         <SiN8N
                         className="text-4xl md:text-6xl"
                         />
                         <p className="text-sm md:text-2xl">N8N</p>
                     </div>
 
-                    <div className="flex flex-col items-center text-[var(--color03)]">
+                    <div className="skill-card flex flex-col items-center text-[var(--color03)]">
                         <RiPhpFill
                         className="text-4xl md:text-6xl"
                         />
                         <p className="text-sm md:text-2xl">PHP</p>
                     </div>
 
-                    <div className="flex flex-col items-center text-[var(--color03)]">
+                    <div className="skill-card flex flex-col items-center text-[var(--color03)]">
                         <FaJsSquare
                         className="text-4xl md:text-6xl"
                         />
                         <p className="text-sm md:text-2xl">JS</p>
                     </div>
 
-                    <div className="flex flex-col items-center text-[var(--color03)]">
+                    <div className="skill-card flex flex-col items-center text-[var(--color03)]">
                         <SiTailwindcss
                         className="text-4xl md:text-6xl"
                         />
                         <p className="text-sm md:text-2xl">Tailwind</p>
                     </div>
 
-                    <div className="flex flex-col items-center text-[var(--color03)]">
+                    <div className="skill-card flex flex-col items-center text-[var(--color03)]">
                         <FaLaravel
                         className="text-4xl md:text-6xl"
                         />
