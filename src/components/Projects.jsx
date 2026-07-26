9cfc4d75b06a14f00934f768885a21fd5f13ad85
lucassant <lucasfdesantana095@gmail.com>
2026-07-25 23:35:15 -0300

    refactor: refatorando componente de projetos

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,7 @@
 import '../index.css'
 import { useState } from 'react'
 
-import listaDeCompras from '../assets/img/projeto-listaDeCompras.png';
+import listaDeCompras from '../assets/img/listaCompras.png';
 import pomodoro from '../assets/img/pomodoro.png';
 
 function Projects(){
@@ -21,7 +21,7 @@ function Projects(){
         image: listaDeCompras,
         link: "https://lucassantdev.github.io/lista-compras/",
         repository: "https://github.com/lucassantDev/lista-compras", 
-        description: "Projeto desenvolvido através do curso da Rocketseat Full-Stack. A ideia principal desse projeto era a manipulação do DOM com JavaScritp"
+        description: "Projeto desenvolvido através do curso da Rocketseat "
         }
     
     ])
@@ -33,30 +33,30 @@ function Projects(){
         <section className=' w-full section-h flex flex-col items-center justify-evenly sm:flex-col'>
             <h1 className='firstSectionFont text-[var(--color00)] text-2xl pb-6 font-semibold md:font-semibold md:text-7xl '>Projetos</h1>
 
-                <div className='flex flex-col md:grid md:grid-cols-2 gap-6 w-2/3'>
+                <div className='w-fit p-1 flex flex-col items-center'>
                     {projects.map(project => (
-                        <div key={project.id} className=''>
-                            <h2 className=''>
-                                {project.title}
-                            </h2>
+                        <div key={project.id} className='w-full border-2 border-[var(--color00)] rounded-md h-7/8 p-2 flex items-center justify-center mt-4 mb-4'>
 
-                            <div className='w-1/2 -h-1/2' style={{ backgroundImage: `url(${project.image})` }}></div>
-                            <p className='text-[var(--color03)] text-center font-semibold text-sm w-full'>
-                                {project.description}
-                            </p>
-
-                            <div className='w-full flex flex-col items-center gap-3 px-4 py-4'>
-                                <div className='flex p-10 flex-col sm:flex-row items-center justify-between w-full gap-3'>
-                                    <div className='flex w-full gap-3 flex-col sm:flex-row'>
-                                        <a href={project.repository} target='_blank'  className=''>Repositório</a>
-                                        <a href={project.link} target='_blank'  className=''>Projeto</a>
-                                    </div>
-
-                                </div>
-
-                                <p className='text-left sm:text-center'>{project.description}</p>
-                                
+                            <div className='w-full h-64 rounded-md flex items-center justify-center'>
+                                <img
+                                    src={project.image}
+                                    alt={project.title}
+                                    className='w-70 h-64 object-cover mt-2 rounded-md border-2 border-[var(--color00)] mb-2'
+                                />
                             </div>
+
+                            <div className='flex flex-col items-left'>
+                                <h2 className='text-2xl font-semibold mb-4 text-[var(--color01)]'>
+                                    {project.title}
+                                </h2>
+
+                                <p className='text-left'>{project.description}</p>
+                                <div className='w-full h-24 flex items-center justify-left text-gray-200'>
+                                    <a href={project.repository} target='_blank'  className='text-center w-36 bg-[var(--color01)] p-2 rounded-md hover:bg-[var(--color04)] hover:text-[var(--color05)]'>Repositório</a>
+                                    <a href={project.link} target='_blank'  className='ml-2 text-center w-24 bg-[var(--color01)] p-2 rounded-md hover:bg-[var(--color04)] hover:text-[var(--color05)]'>Projeto</a>
+                                </div>
+                            </div>
+                                
                         </div>
                     ))}
                 </div>
