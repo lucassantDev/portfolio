2a047695949a662076b6442877f4c05b98182553
lucassant <lucasfdesantana095@gmail.com>
2026-07-27 02:24:38 -0300

    refactor: deixando stacks dinamicas pelo map

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,69 +1,55 @@
-
-
 import { FaReact } from "react-icons/fa";
-import { FiFigma } from "react-icons/fi";
-import { SiN8N } from "react-icons/si";
-import { RiPhpFill } from "react-icons/ri";
-import { FaJsSquare } from "react-icons/fa";
-import { SiTailwindcss } from "react-icons/si";
-import { FaLaravel } from "react-icons/fa6";
-
+import { RiJavascriptFill } from "react-icons/ri";
+import { GrMysql } from "react-icons/gr";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { FaGitAlt } from "react-icons/fa6";
 
 function Skills(){
+
+    const skills = [
+        {
+            id: 1,
+            stack: 'JavaScript',
+            icon: <RiJavascriptFill/>,
+            color: 'text-yellow-400'
+        },
+        {
+            id: 2,
+            stack: 'React',
+            icon: <FaReact/>,
+            color: 'text-blue-600'
+        },
+        {
+            id: 3,
+            stack: 'MySQL',
+            icon: <GrMysql />,
+            color: 'text-gray-500'
+        },
+        {
+            id: 4,
+            stack: 'Git',
+            icon: <FaGitAlt />,
+            color: 'text-orange-600'
+        },
+        {
+            id: 5,
+            stack: 'Tailwind',
+            icon: <RiTailwindCssFill />,
+            color: 'text-cyan-600'
+        },
+
+    ]
+
     return (
         <>
-            <section className="gap-8 w-full h-screen bg-[var(--color01)] flex flex-col items-center justify-center md:gap-12">
-                <h1 className="firstSectionFont text-2xl pb-6 font-semibold text-[var(--color03)] md:font-semibold md:text-7xl">Skills</h1>
-                <div className='w-1/2 grid grid-cols-2 gap-6 md:w-80 md:gap-12'>
-
-                    <div className="skill-card mr-3 flex flex-col items-center text-[var(--color03)]">
-                        <FaReact
-                        className=" text-4xl md:text-6xl"
-                        />
-                        <p className="text-sm md:text-2xl">React</p>
-                    </div>
-
-                    <div className="skill-card mr-4 flex flex-col items-center text-[var(--color03)]">
-                        <FiFigma
-                        className="text-4xl md:text-6xl "
-                        />
-                        <p className="text-sm md:text-2xl">Figma</p>
-                    </div>
-
-                    <div className="skill-card mr-5 flex flex-col items-center text-[var(--color03)]">
-                        <SiN8N
-                        className="text-4xl md:text-6xl"
-                        />
-                        <p className="text-sm md:text-2xl">N8N</p>
-                    </div>
-
-                    <div className="skill-card flex flex-col items-center text-[var(--color03)]">
-                        <RiPhpFill
-                        className="text-4xl md:text-6xl"
-                        />
-                        <p className="text-sm md:text-2xl">PHP</p>
-                    </div>
-
-                    <div className="skill-card flex flex-col items-center text-[var(--color03)]">
-                        <FaJsSquare
-                        className="text-4xl md:text-6xl"
-                        />
-                        <p className="text-sm md:text-2xl">JS</p>
-                    </div>
-
-                    <div className="skill-card flex flex-col items-center text-[var(--color03)]">
-                        <SiTailwindcss
-                        className="text-4xl md:text-6xl"
-                        />
-                        <p className="text-sm md:text-2xl">Tailwind</p>
-                    </div>
-
-                    <div className="skill-card flex flex-col items-center text-[var(--color03)]">
-                        <FaLaravel
-                        className="text-4xl md:text-6xl"
-                        />
-                        <p className="text-sm md:text-2xl">Laravel</p>
-                    </div>
+            <section className=" w-full h-screen border flex flex-col items-center justify-center">
+                <h1 className='text-9xl font-bold bg-gradient-to-r from-emerald-400 to-blue-600 bg-clip-text text-transparent'>Skills</h1>
+                <div className='p-24 flex justify-beetwen'>
+                    {skills.map((skill) =>
+                        <div className='w-48 h-48 flex items-center justify-evenly ml-2 mr-2 shadow-lg transition duration-150 hover:ring-2 hover:ring-blue-400 hover:-translate-y-2 '>
+                            <div className={`text-5xl flex items-center ${skill.color}`}>{skill.icon}</div>
+                        </div>
+                    )}
                 </div>
             </section>
         </>
