973e59e0860d7b8a871ab70e64fc0459f7ba6ba8
lucassant <lucasfdesantana095@gmail.com>
2026-08-02 18:15:38 -0300

    feat: adicionando magic ui no componente

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,19 +1,21 @@
-
+import { TextReveal } from "@/components/ui/text-reveal"
+import { AuroraText } from "@/components/ui/aurora-text"
 
 function AboutMe(){
     return(
         <>
-        <section className='w-full h-dvh flex flex-col items-center before:w-64 before:h-px before:bg-emerald-300 '>
-            <h2 className='text-3xl font-bold text-emerald-300 mt-12 md:text-4xl'>Sobre Mim</h2>
-            {/* <div className='flex items-center border-2 border-emerald-300 rounded-md w-2/3 h-2/3 mt-12 p-10'>
-                <p className='w-2/1 text-left text-clifford wrap-break-word text-3xl font-normal'>
-                    Formado pelo SENAI em 2025, como técnico em Desenvolvimento de Sistemas, procuro aprender
-                    e melhorar de forma contínua, minhas habilidades como desenvolvedor.<br/><br/>
-                    Atualmente tenho 20 anos e trabalho na Empresa Fone Ninja, onde já contribui na automação de processos
-                    com N8N e Google Sheets e desenvolvimento de sistema, com as Stacks PHP, Laravel e Vue.
-
-                </p>
-            </div> */}
+        <section className='border-2 px-2 w-full h-screen flex flex-col justify-center'>
+            <h2 className='text-3xl font-bold text-gray-300 mb-6 md:text-4xl'>Sobre Mim</h2>
+            <AuroraText 
+            className='
+            text-2xl font-semibold
+            md:text-7xl md:font-bold'
+            colors={["oklch(86.5% 0.127 207.078)", "oklch(48.8% 0.243 264.376)"]}
+            >
+                Atualmente trabalho na empresa Fone Ninja como suporte.
+                Em 2025, me formei pelo SENAI como Tecnico em Desenvolvimento de Sistemas!
+                Hoje, com 20 anos, estudo para me tornar um desenvolvedor cada vez melhor, resolvendo problemas e agregando valor.
+            </AuroraText>
         </section>
         </>
         
