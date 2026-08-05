b230293e203a7365cadd7bf4f6e19d9d5fc0384e
lucassant <lucasfdesantana095@gmail.com>
2026-08-05 19:06:24 -0300

    feat: toggle para mostrar descricao e stacks do na section de projeto

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,7 @@
+import {useState, useEffect} from 'react';
 
 function Projects() {
+
 
     // Lista de projetos que será exibida em formato de cards
     const projects = [
@@ -16,10 +18,20 @@ function Projects() {
             title: 'Lista de Compras',
             description: 'Projeto da rocketseat com foco total na interatividade do javascript com a página.',
             test: '',
-            repository: '',
+            repository: 'https://github.com/lucassantDev/lista-compras',
             stack: ['JavaScript', 'HTML', 'CSS']
         }
     ]
+
+    
+    const [projetoClicado, setProjetoClicado] = useState(null)
+    function divProjeto(id){
+        setProjetoClicado((atual) => (atual === id ? null: id))
+    }
+
+    function linkProjeto(link){
+        window.open(link)
+    }
 
     return (
         <>
@@ -27,35 +39,44 @@ function Projects() {
 
                 <h1 className='mb-6 text-3xl font-bold text-gray-300 md:text-4xl'>Projetos</h1>
 
-                <div className='border flex flex-col gap-2 md:w-96'>
+                <div className='border p-8 flex flex-col gap-2 md:w-1/2 '>
                     {projects.map((project, index) => (
                         <div
                             key={project.id ?? index}
                             className='
                             h-64 w-64 shrink-0 rounded-lg border-2 border-slate-700 bg-slate-800 p-4 text-gray-200
-                            md:w-96 md:h-96
+                            md:w-full md:h-fit
                             '
                         >
                             <h2 className='mb-2 text-lg font-semibold'>{project.title}</h2>
-                            <p className='text-sm text-gray-400'>{project.description}</p>
+                            {
+                                projetoClicado === project.id && 
+                                <p className='text-sm text-gray-400'>{project.description}</p>
+                            }
 
-                            {/* Lista das tecnologias usadas no projeto */}
-                            <div className='mt-3 flex flex-wrap gap-2'>
-                                {project.stack.map((tech) => (
-                                    <span key={tech} className='rounded bg-slate-700 px-2 py-1 text-xs'>
-                                        {tech}
-                                    </span>
-                                ))}
-                            </div>
+                            {
+                                projetoClicado === project.id && 
+                                <div className='mt-3 flex flex-wrap gap-2'>
+                                    {project.stack.map((tech) => (
+                                        <span key={tech} className='rounded bg-slate-700 px-2 py-1 text-xs'>
+                                            {tech}
+                                        </span>
+                                    ))}
+                                </div>
+                            }
                             <hr className='mt-2 bg-slate-800'/>
                             <div className='mt-4'>
-                                <a href={project.repository} traget='_blank'><button className='cursor-pointer p-2 bg-blue-500 rounded-sm transition duration-300 hover:-translate-y-1'>projeto</button></a> 
-                                <a href={project.repository} traget='_blank'><button className='cursor-pointer p-2 bg-blue-500 rounded-sm ml-2 transition duration-300 hover:-translate-y-1'>repositório</button></a> 
+                                <button className='cursor-pointer p-2 mr-2 bg-blue-500 rounded-sm transition duration-300 hover:-translate-y-1' onClick={() => divProjeto(project.id)}>
+                                    {projetoClicado === project.id ?  'Fechar' : 'Ver'}
+                                </button>
+                                <button className='cursor-pointer p-2 bg-blue-500 rounded-sm transition duration-300 hover:-translate-y-2' >Projeto</button> 
+                                <button className='cursor-pointer p-2 bg-blue-500 rounded-sm ml-2 transition duration-300 hover:-translate-y-1'>Repositório</button> 
                             </div>
                         </div>
                         
                     ))}
                 </div>
+      
 
             </section>
         </>
