0ebae6a635491afb41b443e7ba7caea8c54cf7ff
lucassant <lucasfdesantana095@gmail.com>
2026-09-01 15:26:23 -0300

    remove: reinicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 // import { useState } from 'react'
-import FirstSection from './components/FirstSection.jsx'
-import AboutMe from './components/AboutMe.jsx'
-import Links from './components/Links.jsx'
-import Skills from './components/Skills.jsx'
-import Projects from './components/Projects.jsx'
-import Footer from './components/Footer.jsx'
+
 
 function App() {
   // const [projetos, setProjetos] = useState('')
 
   return (
     <>
-      <FirstSection/>
-      {/* <Links/> */}
-      <AboutMe/>
-      <Skills/>
-      <Projects/>
-      <Footer/>
+      
     </>
   )
 }
