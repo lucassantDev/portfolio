5f18cc94b967368ba8c14cec9e79230d9db39bce
lucassant <lucasfdesantana095@gmail.com>
2026-09-15 19:14:50 -0300

    feat: adicionando icone de bd

--- src/components/Stacks.jsx
+++ src/components/Stacks.jsx
@@ -3,7 +3,8 @@ import { IoLogoJavascript } from "react-icons/io5";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { FaReact } from "react-icons/fa";
 import { FaGitAlt } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa6";
+import { FaSquareGithub } from "react-icons/fa6";
+import { FaDatabase } from "react-icons/fa";
 
 function Stacks() {
     
@@ -18,7 +19,7 @@ function Stacks() {
             id: 2,
             icone: <IoLogoJavascript />,
             nome: "Javascript",
-            cor: "text-yellow-500"
+            cor: "text-yellow-300"
         },
         {
             id: 3,
@@ -28,32 +29,39 @@ function Stacks() {
         },
         {
             id: 4,
+            icone: <FaDatabase />,
+            nome: "MySQL",
+            cor: "text-gray-600"
+        },
+        {
+            id: 5,
             icone: <FaGitAlt/>,
             nome: "Git",
             cor: "text-orange-500"
         },
         {
-            id: 5,
-            icone: <FaGithub/>,
+            id: 6,
+            icone: <FaSquareGithub />,
             nome: "Github",
             cor: "text-slate-900"
         },
+
        
     ]
 
     return (
         <>
-            <section className="border w-full h-screen">
+            <section className="w-full h-screen">
                 <p className="ml-12 mt-12 mb-12 font-semibold text-gray-300 text-md font-space trasition duration-300 hover:text-gray-600">
                     <Link to="/">Inicio</Link> / <span className="text-gray-400">Tecnologias e Ferramentas</span>
                 </p>
-                <div className="border w-full ">
+                <div className="w-full flex items-center flex-col justify-center">
                     {stacks.map((stack) => (
-                        <div key={stack.id} className="my-2 w-full border flex items-center">
-                            <div className={`ml-6 text-4xl ${stack.cor}`}>
+                        <div key={stack.id} className="my-4 lg:my-6 ml-6 w-full  flex items-center">
+                            <div className={`ml-6 text-4xl lg:text-9xl ${stack.cor}`}>
                                 {stack.icone}
                             </div>
-                            <p className="text-sm font-bold ml-2 text-gray-300">{stack.nome}</p>
+                            <p className="text-5xl font-bold ml-4 text-gray-300 lg:text-9xl lg:ml-8">{stack.nome}</p>
                         </div>
                     ))}
                 </div>
