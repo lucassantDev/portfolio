e21ee094ffe804bffba6413633c2d8538c7e5ca5
lucassant <lucasfdesantana095@gmail.com>
2026-09-22 01:29:02 -0300

    style: margem entre tags de texto

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -9,7 +9,7 @@ function AboutMe() {
                 </p>
                 <div className="w-full h-fit py-12 flex flex-col items-center">
                     <img src={LucasImg} className="lg:mb-8 w-5/6 shadow-md lg:shadow-xl object-cover lg:h-144" alt="Imagem de Lucas Santana" />
-                    <h1 className="mt-6 w-5/6 font-semibold lg:font-extrabold text-xl lg:text-left lg:text-6xl">Sobre Mim</h1>
+                    <h1 className="mt-6 mb-6 w-5/6 font-semibold lg:font-extrabold text-xl lg:text-left lg:text-6xl">Sobre Mim</h1>
                     <p className="w-5/6 lg:text-4xl font-semibold text-gray-500">
                         Sou Lucas Ferreira, tenho 20 anos busco me tornar um desenvolvedor melhor cada dia que passa. <br /> <br/>
                         Conclui o curso técnico de Desenvolvimento de Sistemas pelo SENAI em 2025 onde desenvolvi habilidades solidas em banco de dados, desenvolvimento frontend,
